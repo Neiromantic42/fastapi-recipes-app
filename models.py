972fb419f6255d101102cb59fe93782535b89ea7
Aleksandr Tananov <afrika8759116@gmail.com>
2025-06-24 13:14:55 +0300

    Добавлен конфиг flake8 убирающий конфликт с black models.py, schemas.py, test_main.py отредактированы и приведены к стандарту

--- models.py
+++ models.py
@@ -1,14 +1,15 @@
-from sqlalchemy import Column, String, Integer, Text
+from sqlalchemy import Column, Integer, String, Text
+
 from database import Base
 
-# Модель кулинарной книги будет отображаться в таблицу cookbook
-class CookBook(Base): # Наследует от базового класса
-    __tablename__ = 'cookbook'
-    # Описание столбцов таблицы кулинарной книги
-    id = Column(Integer, primary_key=True, index=True) # Уникальный ID книги, первичный ключ, быстрый доступ к id
-    dish_name = Column(String, unique=True) # Название блюда
-    cooking_time = Column(Integer) # Время приготовления блюда
-    ingredients = Column(Text) # список ингредиентов
-    description_dish = Column(Text) # текстовое описание блюда
-    views_counter = Column(Integer, default=0) # Счетчик просмотров конкретного рецепта
 
+#  Модель кулинарной книги будет отображаться в таблицу cookbook
+class CookBook(Base):  # Наследует от базового класса
+    __tablename__ = "cookbook"
+    # Описание столбцов таблицы кулинарной книги
+    id = Column(Integer, primary_key=True, index=True)  # Уникальный ID книги
+    dish_name = Column(String, unique=True)  # Название блюда
+    cooking_time = Column(Integer)  # Время приготовления блюда
+    ingredients = Column(Text)  # список ингредиентов
+    description_dish = Column(Text)  # текстовое описание блюда
+    views_counter = Column(Integer, default=0)  # Счетчик просмотров
--- test_main.py
+++ test_main.py
@@ -1,6 +1,7 @@
-import pytest
 import random
+
 from fastapi.testclient import TestClient
+
 from main import app
 
 client = TestClient(app)
@@ -10,18 +11,26 @@ def test_post_create_recipe():
     response = client.post(
         "/create/recipe/",
         json={
-            "dish_name": "Test Dish {name}".format(name=random.randint(1, 1000)),
+            "dish_name": f"Test Dish {random.randint(1, 1000)}",
             "cooking_time": 15,
             "ingredients": "Test ingredients",
-            "description_dish": "Test description"
-        }
+            "description_dish": "Test description",
+        },
     )
     assert response.status_code == 200
     data = response.json()
-    assert all(key in data for key in [
-        "dish_name", "cooking_time", "ingredients",
-        "description_dish", "id", "views_counter"
-    ])
+    assert all(
+        key in data
+        for key in [
+            "dish_name",
+            "cooking_time",
+            "ingredients",
+            "description_dish",
+            "id",
+            "views_counter",
+        ]
+    )
+
 
 def test_get_recipies():
     response = client.get("/recipes")
@@ -36,7 +45,8 @@ def test_get_recipies():
         elif current["views_counter"] == next_item["views_counter"]:
             assert current["cooking_time"] <= next_item["cooking_time"]
         else:
-            assert False, "Сортировка по кол-ву просмотров нарушена"
+            raise AssertionError("Сортировка по кол-ву просмотров нарушена")
+
 
 def test_get_detailed_recipe():
     recipe_id = 2
@@ -49,12 +59,17 @@ def test_get_detailed_recipe():
     next_views_counter = data_2["views_counter"]
     assert current_views_counter < next_views_counter
 
+
 def test_error_handling_get_recipe():
     non_existent_id = 100
     response = client.get(f"/recipes/{non_existent_id}")
     assert response.status_code == 404
     data = response.json()
-    assert data['detail'] == f"Рецепт с id {non_existent_id} не существует. Пожалуйста, укажите существующий id"
+    assert (
+        data["detail"] == f"Рецепт с id {non_existent_id} не существует."
+        f" Пожалуйста, укажите существующий id"
+    )
+
 
 def test_recipe_creation_error_handling():
     response = client.post(
@@ -63,7 +78,7 @@ def test_recipe_creation_error_handling():
             "dish_name": "Test Dish",
             "cooking_time": 15,
             "ingredients": 100,  # Не валидные данные
-            "description_dish": "Test description"
-        }
+            "description_dish": "Test description",
+        },
     )
     assert response.status_code == 422
